c761db42a16d2dac087afb07e38377dd6272ea1b
Matthias Leuffen <matthes@leuffen.de>
2024-04-17 11:57:12 +0200

    sync

--- src/CR.php
+++ src/CR.php
@@ -4,6 +4,7 @@ namespace Brix\Coder;
 
 use Brix\Coder\Manager\ChangeRequestManager;
 use Brix\Coder\Manager\CoderManager;
+use Brix\Coder\Type\T_CoderConfig;
 use Brix\Core\AbstractBrixCommand;
 use Phore\Cli\Input\In;
 
@@ -13,6 +14,7 @@ class CR extends AbstractBrixCommand
     public function __construct()
     {
         parent::__construct();
+        $this->brixEnv->brixConfig->get("coder", T_CoderConfig::class, file_get_contents(__DIR__ . "/config_tpl.yml"));
         $this->manager = new ChangeRequestManager($this->brixEnv);
     }
 
--- src/Coder.php
+++ src/Coder.php
@@ -3,6 +3,7 @@
 namespace Brix\Coder;
 
 use Brix\Coder\Manager\CoderManager;
+use Brix\Coder\Type\T_CoderConfig;
 use Brix\Core\AbstractBrixCommand;
 use Phore\Cli\CLIntputHandler;
 
@@ -13,6 +14,7 @@ class Coder extends AbstractBrixCommand
     public function __construct()
     {
         parent::__construct();
+        $this->brixEnv->brixConfig->get("coder", T_CoderConfig::class, file_get_contents(__DIR__ . "/config_tpl.yml"));
         $this->manager = new CoderManager($this->brixEnv);
     }
 
